cd9f3a5a3f9c28a63ea2b2a08fb96803315aa8be
babji <babji.mlogisoft@gmail.com>
2022-12-15 15:40:05 +0530

    first commit

---
+++ src/config/connection.js
@@ -0,0 +1,56 @@
+const dbconfig= require("./dbconfig")
+const {Sequelize}=require('sequelize')
+var path = require('path');
+
+const sequelize=new Sequelize (
+
+    dbconfig.DB,
+    dbconfig.USER,
+    dbconfig.PASSWORD,{
+       host : dbconfig.Host,
+       dialect:dbconfig.DIALECT
+    }
+
+)
+sequelize.authenticate()
+.then(()=>{
+    console.log('Connected')
+    
+}).catch(err=>{
+    console.log('Error'+err)
+})
+
+//dbconnection loop
+
+var filesArray =[
+    '../models/roles',
+    '../models/employee',
+    '../routes/index/employee'
+]
+const db={};
+
+filesArray.forEach((file) =>{
+
+    var model =require(path.join(__dirname,file))(
+        sequelize,
+        Sequelize.DataTypes
+    );
+    db[model.name]= model;
+
+});
+Object.keys(db).forEach((modelName)=>{
+    console.log(db[modelName]);
+    if(db[modelName].associate){
+        db[modelName].associate(db);
+    }
+})
+
+db.Sequelize = Sequelize;
+    
+db.sequelize = sequelize;
+
+sequelize.sync ({force : true})
+.then(()=>{
+    console.log('tables created')
+})
+ module.exports = db
